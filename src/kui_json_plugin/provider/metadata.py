de3b36734d68d3a8426ac63753b60be68ebf856f
Artur Drabazha <jhorr2222@gmail.com>
2026-01-13 19:24:26 +0200

    Refactor metadata provider

--- src/kui_json_plugin/provider/metadata.py
+++ src/kui_json_plugin/provider/metadata.py
@@ -3,6 +3,7 @@ import os
 from kui.core.metadata import WidgetMetadata
 from kui.core.service.provider import MetadataProvider
 from kui.core.shortcut import resolve_config
+from kui.transformer.widget import JSONWidgetDataTransformer
 from kutil.file import read_file
 from kutil.file_type import JSON
 
@@ -11,16 +12,25 @@ class JsonMetadataProvider(MetadataProvider):
 
     def provide(self, section_id: str) -> list[WidgetMetadata]:
 
-        metadata = []
         section_file_name = JSON.add_extension(section_id)
         metadata_file_path = resolve_config("widgets", section_file_name)
 
         if not os.path.exists(metadata_file_path):
-            return metadata
+            return []
 
-        metadata_json = read_file(metadata_file_path, as_json=True)
+        transformer = JSONWidgetDataTransformer()
+        nested_data = read_file(metadata_file_path, as_json=True)
+        widget_data = transformer.flatten(nested_data)
+        metadata = []
 
-        for widget in metadata_json:
+        for widget in widget_data:
+
+            stylesheet = widget.get("stylesheet", "{}")
+            stylesheet = self._parse_stylesheet(stylesheet)
+
+            if stylesheet:
+                widget["stylesheet"] = stylesheet
+
             widget_meta = WidgetMetadata(**widget)
             metadata.append(widget_meta)
 
